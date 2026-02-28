4d32dfd96a3e3328de2628baf849b82de2a67616
foropo05 <foropo@my.centennialcollege.ca>
2026-02-27 22:12:50 -0500

    Fixed update and delete car endpoints

--- config/db.js
+++ config/db.js
@@ -1,7 +1,6 @@
 require('dotenv').config()
 const mongoose = require('mongoose');
 
-let ConnectionString = "mongodb+srv://foropo_db_user:Ayomide09@cluster0.7vmxa3j.mongodb.net/Midterm?appName=Cluster002"
 
 const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 
